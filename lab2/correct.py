6a4fa5f8600c038caccbcf486338af012f813061
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-20 18:44:35 +0100

    [lab2] Errors done

--- lab2/correct.py
+++ lab2/correct.py
@@ -9,6 +9,8 @@ from metric import levenshtein_metric
 
 __author__ = 'Michał Ciołczyk'
 
+MAX_LENGTH_DIFFERENCE = 3
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: %s word_to_correct' % sys.argv[0])
@@ -20,11 +22,13 @@ if __name__ == '__main__':
 
     time1 = time()
     for word2 in get_data_contents():
-        if abs(len(word1) - len(word2)) <= 5:
+        if abs(len(word1) - len(word2)) <= MAX_LENGTH_DIFFERENCE:
             dist = levenshtein_metric(word1, word2, False)
             if dist < best_word_dist:
                 best_word = word2
                 best_word_dist = dist
+            if dist == 0:
+                break
     time2 = time()
 
     print("%s: %s, dist=%f" % (word1, best_word, best_word_dist))
--- lab2/metric.py
+++ lab2/metric.py
@@ -7,23 +7,102 @@ DELETE_COST = 1
 INSERT_COST = 1
 CHANGE_COST = 1
 CZECH_ERROR_COST = 0.5
+DIACRITIC_ERROR_COST = 0.25
+SPELLING_ERROR_COST = 0.5
+
+COSTS_OF_MISTAKES = {
+    'ą': [
+        ('a', DIACRITIC_ERROR_COST),  # cost of a <-> ą is DIACRITIC_ERROR_COST
+        ('on', SPELLING_ERROR_COST - CHANGE_COST)  # cost of a <-> on is SPELLING_ERROR_COST,
+        #                                            but on the 'n' letter it will find it one more time
+        #                                            - so we need to subtract
+    ],
+    'ć': [
+        ('c', DIACRITIC_ERROR_COST)
+    ],
+    'ę': [
+        ('e', DIACRITIC_ERROR_COST),
+        ('en', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'ł': [
+        ('l', DIACRITIC_ERROR_COST)
+    ],
+    'ń': [
+        ('n', DIACRITIC_ERROR_COST)
+    ],
+    'ó': [
+        ('o', DIACRITIC_ERROR_COST)
+    ],
+    'ś': [
+        ('s', DIACRITIC_ERROR_COST),
+        ('si', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'ż': [
+        ('z', DIACRITIC_ERROR_COST),
+        ('rz', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'ź': [
+        ('z', DIACRITIC_ERROR_COST)
+    ],
+    'h': [
+        ('ch', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'u': [
+        ('ó', SPELLING_ERROR_COST)
+    ],
+    'c': [
+        ('dz', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'łu': [
+        ('u', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'trz': [
+        ('cz', SPELLING_ERROR_COST - 2 * CHANGE_COST),
+        ('tsz', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+    'wsz': [
+        ('fsz', SPELLING_ERROR_COST - CHANGE_COST)
+    ],
+}
 
 
 # noinspection PyTypeChecker
 def levenshtein_metric(word1, word2, basic_metric=True):
     lev = [[max(i, j) if min(i, j) == 0 else 0 for j in range(len(word2) + 1)] for i in range(len(word1) + 1)]
+    costs = [[CHANGE_COST for _ in range(len(word2) + 1)] for __ in range(len(word1) + 1)]
 
+    if not basic_metric:
+        for i in range(len(word1)):
+            for j in range(len(word2)):
+                if word1[i] == word2[j] and costs[i][j] == CHANGE_COST:
+                    costs[i][j] = 0
+                if i > 1 and j > 1 and word1[i - 1] == word2[j] and word2[j - 1] == word1[i]:
+                    costs[i - 1][j - 1] = CHANGE_COST
+                    costs[i][j] = -0.5
+                else:
+                    check1 = word1[i:]
+                    check2 = word2[j:]
+                    for token, possible_mistakes in COSTS_OF_MISTAKES.items():
+                        if check1.startswith(token):
+                            for mistake_token, mistake_cost in possible_mistakes:
+                                if check2.startswith(mistake_token):
+                                    current_cost = costs[i + len(token) - 1][j + len(mistake_token) - 1]
+                                    costs[i + len(token) - 1][j + len(mistake_token) - 1] = min(mistake_cost,
+                                                                                                current_cost)
+
+                        if check2.startswith(token):
+                            for mistake_token, mistake_cost in possible_mistakes:
+                                if check1.startswith(mistake_token):
+                                    current_cost = costs[i + len(mistake_token) - 1][j + len(token) - 1]
+                                    costs[i + len(mistake_token) - 1][j + len(token) - 1] = min(mistake_cost,
+                                                                                                current_cost)
     for i in range(1, len(word1) + 1):
         for j in range(1, len(word2) + 1):
-            aiaj1 = 0 if word1[i - 1] == word2[j - 1] else 1
-            # Idea taken from: https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
-            if i > 1 and j > 1 and word1[i - 2] == word2[j - 1] and word1[i - 1] == word2[j - 2] and not basic_metric:
-                lev[i][j] = min(lev[i - 1][j] + DELETE_COST, lev[i][j - 1] + INSERT_COST,
-                                lev[i - 1][j - 1] + aiaj1 * CHANGE_COST,
-                                lev[i - 2][j - 2] + CZECH_ERROR_COST)
-            else:
-                lev[i][j] = min(lev[i - 1][j] + DELETE_COST, lev[i][j - 1] + INSERT_COST,
-                                lev[i - 1][j - 1] + aiaj1 * CHANGE_COST)
+            are_same = 0 if word1[i - 1] == word2[j - 1] else 1
+            lev[i][j] = min(lev[i - 1][j] + DELETE_COST,
+                            lev[i][j - 1] + INSERT_COST,
+                            lev[i - 1][j - 1] + are_same * costs[i - 1][j - 1])
+
     return lev[len(word1)][len(word2)]
 
 
@@ -31,4 +110,13 @@ if __name__ == "__main__":
     print(levenshtein_metric("biurko", "pióro"))
     print(levenshtein_metric("biurko", "biurko"))
     print(levenshtein_metric("biurko", "biukro"))
+    print(levenshtein_metric("biurko", "biórko"))
+    print(levenshtein_metric("testując", "testujac"))
+    print(levenshtein_metric("biurko", "biukro"))
+    print("---")
+    print(levenshtein_metric("biurko", "pióro", False))
+    print(levenshtein_metric("biurko", "biurko", False))
     print(levenshtein_metric("biurko", "biukro", False))
+    print(levenshtein_metric("biurko", "biukro", False))
+    print(levenshtein_metric("biurko", "biórko", False))
+    print(levenshtein_metric("testując", "testujac", False))
