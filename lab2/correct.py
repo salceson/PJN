2535a12667bddca57a94aff46bba947d74f3e869
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-22 11:41:22 +0100

    Optimized words in dictionary

--- lab2/correct.py
+++ lab2/correct.py
@@ -21,14 +21,16 @@ if __name__ == '__main__':
     best_word = word1
 
     time1 = time()
-    for word2 in get_data_contents():
-        if abs(len(word1) - len(word2)) <= MAX_LENGTH_DIFFERENCE:
-            dist = levenshtein_metric(word1, word2, False, best_word_dist)
-            if dist < best_word_dist:
-                best_word = word2
-                best_word_dist = dist
-            if dist == 0:
-                break
+    dictionary = get_data_contents()
+    if word1 not in dictionary:
+        for word2 in dictionary:
+            if abs(len(word1) - len(word2)) <= MAX_LENGTH_DIFFERENCE:
+                dist = levenshtein_metric(word1, word2, False, best_word_dist)
+                if dist < best_word_dist:
+                    best_word = word2
+                    best_word_dist = dist
+    else:
+        best_word_dist = 0
     time2 = time()
 
     print("%s: %s, dist=%f" % (word1, best_word, best_word_dist))
