4eb038670ff1c916d7b2452ae29584383474b95f
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-23 16:25:49 +0100

    [lab2] All good words

--- lab2/correct.py
+++ lab2/correct.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     word1 = sys.argv[1]
     best_word_dist = 9999999
-    best_word = word1
+    best_word_fits = [word1]
 
     time1 = time()
     dictionary = get_data_contents()
@@ -27,11 +27,13 @@ if __name__ == '__main__':
             if abs(len(word1) - len(word2)) <= MAX_LENGTH_DIFFERENCE:
                 dist = levenshtein_metric(word1, word2, False, best_word_dist)
                 if dist < best_word_dist:
-                    best_word = word2
+                    best_word_fits = [word2]
                     best_word_dist = dist
+                elif dist == best_word_dist:
+                    best_word_fits.append(word2)
     else:
         best_word_dist = 0
     time2 = time()
 
-    print("%s: %s, dist=%f" % (word1, best_word, best_word_dist))
+    print("%s: %s, dist=%f" % (word1, best_word_fits, best_word_dist))
     print("Run for %f seconds." % (time2 - time1))
