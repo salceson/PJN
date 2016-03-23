e5af222ffc0b7fed2dc77e734811fc7320dcb4d6
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-23 12:22:10 +0100

    [lab2] Stats

--- lab2/test.py
+++ lab2/test.py
@@ -5,4 +5,12 @@ import subprocess
 
 __author__ = 'Michał Ciołczyk'
 
-
+if __name__ == "__main__":
+    words = ["pomidor", "pomdior", "dupeczka", "rozwiac", "abzzur", "chumanistyzcny"]
+    interpreters = ["python3", "pypy3"]
+    for interpreter in interpreters:
+        print("Interpreter:", interpreter)
+        for word in words:
+            p = subprocess.Popen([interpreter, "correct.py", word], stdout=subprocess.PIPE)
+            result = p.communicate()[0].decode("utf-8").replace("\n", "\n\t")
+            print("\t%s" % str(result))
